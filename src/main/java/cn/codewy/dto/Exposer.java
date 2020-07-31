1acba3e553d8d6be7dc25dfb50b2f672b8db56c0
Code-wy <wy17851351855@163.com>
2020-07-31 14:27:30 +0800

    封装操作结果属性

---
+++ src/main/java/cn/codewy/dto/Exposer.java
@@ -0,0 +1,107 @@
+package cn.codewy.dto;
+
+/**
+ * 暴露选课地址DTO
+ *
+ * @auther CodeWy
+ * @date 2020/3/8
+ */
+public class Exposer {
+
+    //是否开启选课
+    private boolean exposed;
+
+    //加密措施，避免用户通过抓包拿到地址
+    private String md5;
+
+    //ID
+    private long seckillId;
+
+    //系统当前时间（毫秒）
+    private long now;
+
+    //秒杀开启时间
+    private long start;
+
+    //选课结束时间
+    private long end;
+
+    public Exposer(boolean exposed, String md5, long seckillId) {
+        this.exposed = exposed;
+        this.md5 = md5;
+        this.seckillId = seckillId;
+    }
+
+    public Exposer(boolean exposed, Long seckillId, long now, long start, long end) {
+        this.exposed = exposed;
+        this.seckillId = seckillId;
+        this.now = now;
+        this.start = start;
+        this.end = end;
+    }
+
+    public Exposer(boolean exposed, long seckillId) {
+        this.exposed = exposed;
+        this.seckillId = seckillId;
+    }
+
+    public boolean isExposed() {
+        return exposed;
+    }
+
+    public void setExposed(boolean exposed) {
+        this.exposed = exposed;
+    }
+
+    public String getMd5() {
+        return md5;
+    }
+
+    public void setMd5(String md5) {
+        this.md5 = md5;
+    }
+
+    public long getSeckillId() {
+        return seckillId;
+    }
+
+    public void setSeckillId(long seckillId) {
+        this.seckillId = seckillId;
+    }
+
+    public long getNow() {
+        return now;
+    }
+
+    public void setNow(long now) {
+        this.now = now;
+    }
+
+    public long getStart() {
+        return start;
+    }
+
+    public void setStart(long start) {
+        this.start = start;
+    }
+
+    public long getEnd() {
+        return end;
+    }
+
+    public void setEnd(long end) {
+        this.end = end;
+    }
+
+    @Override
+    public String toString() {
+        return "Exposer{" +
+                "exposed=" + exposed +
+                ", md5='" + md5 + '\'' +
+                ", seckillId=" + seckillId +
+                ", now=" + now +
+                ", start=" + start +
+                ", end=" + end +
+                '}';
+    }
+}
